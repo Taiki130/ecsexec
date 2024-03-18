ecfc643eb56de6d27089aae30cbde2db36630ecc
Taiki130 <air19990130@yahoo.co.jp>
2024-03-18 23:34:09 +0900

    selectCluster

--- main.go
+++ main.go
@@ -9,41 +9,54 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"os/signal"
 	"strings"
-	"syscall"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/ecs"
 	"github.com/aws/aws-sdk-go-v2/service/ecs/types"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/manifoldco/promptui"
 )
 
 func main() {
-	cluster := flag.String("cluster", "", "ECS cluster name")
-	service := flag.String("service", "", "ECS service name")
-	container := flag.String("container", "", "container name")
-	region := flag.String("region", "ap-northeast-1", "AWS Region")
-	command := flag.String("command", "/bin/sh", "execute command")
-	profile := flag.String("profile", "", "aws profile")
+	cluster := flag.String("cluster", "", "the name of your ECS cluster.")
+	service := flag.String("service", "", "the name of your ECS service.")
+	container := flag.String("container", "", "the name of container name.")
+	region := flag.String("region", "", "AWS Region.")
+	command := flag.String("command", "/bin/sh", "Specify the command to execute. Default: /bin/sh")
+	profile := flag.String("profile", "", "AWS profile to use.")
 
 	flag.Parse()
 
+	if *region == "" {
+		regionVar, ok := os.LookupEnv("AWS_REGION")
+		if !ok {
+			fmt.Print("Enter region: ")
+			fmt.Scanf("%s", &region)
+		} else {
+			*region = regionVar
+		}
+	}
+
 	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(*region), config.WithSharedConfigProfile(*profile))
 	if err != nil {
-		log.Fatalf("設定情報の読み込みに失敗しました。: %w", err)
+		log.Fatalf("Failed to load configuration: %w", err)
 	}
 	client := ecs.NewFromConfig(cfg)
 
+	if *cluster == "" {
+		selectCluster(client)
+	}
+
 	taskID, err := getTaskID(client, *cluster, *service)
 	if err != nil {
-		log.Fatalf("task ID の取得に失敗しました。: %w", err)
+		log.Fatalf("Failed to retrieve task ID: %w", err)
 	}
 
 	runtimeID, err := getRuntimeID(client, taskID, *cluster, *container)
 	if err != nil {
-		log.Fatalf("runtime ID の取得に失敗しました。: %w", err)
+		log.Fatalf("Failed to retrieve runtime ID: %w", err)
 	}
 
 	resp, err := client.ExecuteCommand(context.TODO(), &ecs.ExecuteCommandInput{
@@ -59,6 +72,31 @@ func main() {
 	err = startSession(resp.Session, *region, target)
 }
 
+func selectCluster(client *ecs.Client) (string, error) {
+	l := "Select cluster"
+	resp, err := client.ListClusters(context.TODO(), &ecs.ListClustersInput{})
+	if err != nil {
+		return "", err
+	}
+	clusterArns := resp.ClusterArns
+	if len(clusterArns) == 0 {
+		return "", errors.New("no ECS cluster found.")
+	}
+	var i []string
+	for _, arn := range clusterArns {
+		i = append(i, strings.Split(arn, "/")[1])
+	}
+	prompt := promptui.Select{
+		Label: l,
+		Items: i,
+	}
+	_, result, err := prompt.Run()
+	if err != nil {
+		return "", err
+	}
+	return result, nil
+}
+
 func getTaskID(client *ecs.Client, cluster, service string) (string, error) {
 	resp, err := client.ListTasks(context.TODO(), &ecs.ListTasksInput{
 		Cluster:     aws.String(cluster),
@@ -69,7 +107,7 @@ func getTaskID(client *ecs.Client, cluster, service string) (string, error) {
 	}
 	taskArns := resp.TaskArns
 	if len(taskArns) == 0 {
-		return "", errors.New("TaskArnsが取得できませんでした")
+		return "", errors.New("No ECS task found.")
 	}
 	taskID := strings.Split(taskArns[0], "/")[2]
 	return taskID, nil
@@ -109,7 +147,6 @@ func startSession(sess *types.Session, region string, target string) error {
 	cmd.Stdout = os.Stdout
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
-	signal.Ignore(syscall.SIGINT)
 	cmd.Run()
 	return nil
 }
